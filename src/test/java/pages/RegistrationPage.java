0df4bf778cbd6c9d2083d2e4e3dec769e63ca626
Прокопьев Дмитрий Викторович (4080425) <prokopevd@dev.vtb.ru>
2023-05-01 04:25:42 +0400

    init

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import pages.components.CalendarComponent;
 import pages.components.RegistrationResultsModal;
 
@@ -27,12 +28,14 @@ public class RegistrationPage {
             cityInput = $("#react-select-4-input"),
             submitBtn = $("#submit");
 
+    @Step("Открываем страницу формы")
     public RegistrationPage openPage() {
         open("/automation-practice-form");
 
         return this;
     }
 
+    @Step("Удаляем баннеры")
     public RegistrationPage removeBanners() {
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
@@ -40,36 +43,42 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Указываем имя")
     public RegistrationPage setFirstName(String value) {
         firstNameInput.setValue(value);
 
         return this;
     }
 
+    @Step("Указываем фамилию")
     public RegistrationPage setLastName(String value) {
         lastNameInput.setValue(value);
 
         return this;
     }
 
+    @Step("Указываем e-mail")
     public RegistrationPage setEmail(String value) {
         emailInput.setValue(value);
 
         return this;
     }
 
+    @Step("Указываем пол")
     public RegistrationPage setGender(String value) {
         genderWrapper.$(byText(value)).click();
 
         return this;
     }
 
+    @Step("Указываем номер")
     public RegistrationPage setNumber(String value) {
         numberInput.setValue(value);
 
         return this;
     }
 
+    @Step("Указываем дату рождения")
     public RegistrationPage setBirthDate(String day, String month, String year) {
         birthDateInput.click();
         calendarComponent.setDate(day, month, year);
@@ -77,54 +86,63 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Указываем скиллы")
     public RegistrationPage setSubject(String value) {
         subjectInput.setValue(value).pressEnter();
 
         return this;
     }
 
+    @Step("Указываем хобби")
     public RegistrationPage setHobbies(String value) {
         hobbiesWrapper.$(byText(value)).click();
 
         return this;
     }
 
+    @Step("Загружаем файл")
     public RegistrationPage uploadFile(String value) {
         fileInput.uploadFromClasspath(value);
 
         return this;
     }
 
+    @Step("Указываем адрес")
     public RegistrationPage setAddress(String value) {
         addressInput.setValue(value);
 
         return this;
     }
 
+    @Step("Указываем штат")
     public RegistrationPage setState(String value) {
         stateInput.setValue(value).pressEnter();
 
         return this;
     }
 
+    @Step("Указываем город")
     public RegistrationPage setCity(String value) {
         cityInput.setValue(value).pressEnter();
 
         return this;
     }
 
+    @Step("Отправляем форму")
     public RegistrationPage clickSubmit() {
         submitBtn.click();
 
         return this;
     }
 
+    @Step("Проверяем, что модальное окно появилось")
     public RegistrationPage verifyResultsModalAppears() {
         registrationResultsModal.verifyModalAppears();
 
         return this;
     }
 
+    @Step("Проверяем результат в модальном окне")
     public RegistrationPage verifyResult(String key, String value) {
         registrationResultsModal.verifyResult(key, value);
 
